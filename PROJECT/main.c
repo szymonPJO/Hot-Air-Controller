0b48097208bdaf15252543b105e43e6163424982
SzymonPJO <szymonpj64@gmail.com>
2023-10-07 09:37:59 +0200

    blink led - test

--- PROJECT/main.c
+++ PROJECT/main.c
@@ -1,11 +1,14 @@
-#include <stdlib.h>
+// #include <stdlib.h>
 #include <avr/io.h>
-// #include <util/delay.h>
-// #include <avr/interrupt.h>
-// #include <avr/sleep.h>
+#include <util/delay.h>
 
 #define F_CPU 8000000UL
 
 int main(void){
- 
+ 	DDRB = 	0xFF;
+    while(1){
+        // for (uint32_t i = 100000; i>0; i--);
+        _delay_ms(1000);
+        PORTB ^= 0xFF;
+    }
 }
